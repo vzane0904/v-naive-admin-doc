1a194e154f2ad03011867b9ff006ee223088a32b
yc6 <zane0904@88.com>
2022-08-18 16:23:38 +0800

    init

---
+++ src/components/middle.vue
@@ -0,0 +1,20 @@
+<template>
+  <div class="middle">
+    <h2 className="message"></h2>
+  </div>
+</template>
+<script lang="ts" setup></script>
+<style lang="scss" scoped>
+.middle {
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  margin-top: 80px;
+  color: var(--vp-c-brand);
+  cursor: pointer;
+  text-decoration: underline;
+  &:hover {
+    color: var(--vp-c-brand-dark);
+  }
+}
+</style>
